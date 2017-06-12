1eb5f9d2d0573a2eb8b25ad9ccf9c3dd59ce2bde
Nicolas Solioz <Nicolas Solioz@DESKTOP-1E89E9L>
2017-06-12 20:48:09 +0200

    Create PanelImage

---
+++ VouillozSolioz/src/application/gallery/PanelImage.java
@@ -0,0 +1,11 @@
+package application.gallery;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public class PanelImage extends JPanel {
+
+	public PanelImage(int nbImage, JPanel images, JScrollPane pnl) {
+		System.out.println("yes");
+	}
+}
