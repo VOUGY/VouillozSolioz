d0ce1d415792e47d7bc49a5d5cbdbbff322dd251
VOUGY <guillaume.vouilloz@outlook.com>
2017-06-15 12:49:41 +0200

    Decomment class contacts + del contact solioz + add images

--- VouillozSolioz/src/main/Main.java
+++ VouillozSolioz/src/main/Main.java
@@ -24,7 +24,7 @@ public class Main {
 		// Create panels (one panel per app)
 		PanelHome pnlHome = new PanelHome();
 		PanelGallery pnlGallery = new PanelGallery();
-		//Contacts pnlContacts = new Contacts();
+		Contacts pnlContacts = new Contacts();
 		
 		//Initalize shell of our homescreen
 		FrameShell myShell = new FrameShell(pnlHome);
@@ -32,7 +32,7 @@ public class Main {
 		//Add panels (apps) to the shell
 
 		FrameShell.addPnlScreen(pnlGallery,"Gallery");
-		//FrameShell.addPnlScreen(pnlContacts,"Contact");
+		FrameShell.addPnlScreen(pnlContacts,"Contact");
 		
 
 	
